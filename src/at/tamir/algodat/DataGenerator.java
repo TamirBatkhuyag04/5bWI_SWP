b9d6b102d0b9feab7790009736ee1eeaec62467d
Tamir <tamir@htld>
2024-11-28 12:07:57 +0100

    DataGenerator: done | 28.11.2024

---
+++ src/at/tamir/algodat/DataGenerator.java
@@ -0,0 +1,30 @@
+package at.tamir.algodat;
+
+import java.util.Random;
+
+public class DataGenerator {
+    public static int[] generateDataArray(int size) {
+        Random rand = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = rand.nextInt();
+        }
+        return data;
+    }
+
+    public static int[] generateDataArray2(int size, int min, int max) {
+        Random rand = new Random();
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = rand.nextInt(max + 1 - min) + min;
+            // * max+1 damit die höchste Grenze includiert wird
+        }
+        return data;
+    }
+
+    public static void printArray(int[] data) {
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/at/tamir/algodat/DataGeneratorTest.java
@@ -0,0 +1,41 @@
+package at.tamir.algodat;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataGeneratorTest {
+
+    @Test
+    void testGenerateDataArray() {
+        int size = 10;
+        int[] result = DataGenerator.generateDataArray(size);
+
+        // Test 1: Größe des Arrays überprüfen
+        assertEquals(size, result.length);
+    }
+
+    @Test
+    void testGenerateDataArray2() {
+        int size = 10;
+        int min = 5;
+        int max = 15;
+        int[] result = DataGenerator.generateDataArray2(size, min, max);
+
+        // Test 1: Größe des Arrays überprüfen
+        assertEquals(size, result.length);
+
+        // Test 2: Sicherstellen, dass Werte innerhalb der Grenzen liegen
+        for (int num : result) {
+            assertTrue(num >= min && num <= max);
+        }
+    }
+
+    @Test
+    void testPrintArray() {
+        int[] data = { 1, 2, 3, 4, 5 };
+        String expectedOutput = "1 2 3 4 5 ";
+        assertDoesNotThrow(() -> {
+            DataGenerator.printArray(data);
+        });
+    }
+}
